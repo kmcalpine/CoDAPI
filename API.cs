456f0839574b3ba3e4627b23df45bc15d173e280
Kurtis McAlpine <kurtismcalpine@gmail.com>
2021-04-02 05:31:17 +0100

    First commit

---
+++ API.cs
@@ -0,0 +1,104 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodApi
+{
+    public class Api
+    {
+        public Authenticate auth { get; set; }
+        public Api()
+        {
+        }
+
+        public void Auth(ref Authenticate _auth)
+        {
+            auth = _auth;
+        }
+
+        //Private Routes
+        public async Task<Object> Identity(string version)
+        {
+            // crm/cod/:version/identities
+            string endpoint = "crm/cod/" + version +
+                              "/identities";
+
+            return await AuthenticatedRequest(endpoint);
+        }
+
+        public async Task<Object> UserInfo()
+        {
+            // profile.callofduty.com/cod/userInfo/{{TOKEN_SSO}}
+            var client = new RestClient("https://profile.callofduty.com/cod/userInfo/MjE1MTI1NjM0NTk5OTcwMjk3OToxNjE4NTMyMjY5MTAzOjc5NGUxYjZhMWJiMTMzZDEwYWE2YmI2YTQ4NGEyZThk" + auth.sso);
+            client.Timeout = -1;
+
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("content-type", "application/json");
+
+            var queryResult = await client.ExecuteAsync<Object>(request);
+            return queryResult.Data;
+        }
+
+        public async Task<Object> SearchPlayer(string platform, string user)
+        {
+            // crm/cod/v2/platform/:platform/username/:username/search
+            string endpoint = "crm/cod/v2/platform/" + platform +
+                                "/username/" + user +
+                                "/search";
+
+            return await AuthenticatedRequest(endpoint);
+        }
+
+
+        //Protected Routes
+        public async Task<Object> Platforms(string version, string platform, string user)
+        {
+            // crm/cod/:version/accounts/platform/:platform/gamer/:username
+            string endpoint = "crm/cod/" + version +
+                                "/accounts/platform/" + platform +
+                                "/gamer/" + user;
+
+            return await AuthenticatedRequest(endpoint);
+        }
+
+        public async Task<Object> Profile(string version, string game, string platform, string user, string mode)
+        {
+            // stats/cod/:version/title/:game/platform/:platform/gamer/:username/profile/type/:mode
+            string endpoint = "stats/cod/" + version +
+                                "/title/" + game +
+                                "/platform/" + platform +
+                                "/gamer/" + user +
+                                "/profile/type/" + mode;
+
+            return await AuthenticatedRequest(endpoint);
+        }
+        public async Task<Object> Matches(string version, string game, string platform, string user, string mode, string start, string end)
+        {
+            // crm/cod/:version/title/:game/platform/:platform/gamer/:username/matches/:mode/start/:start/end/:end/details
+            string endpoint = "crm/cod/" + version +
+                                "/title/" + game +
+                                "/platform/" + platform +
+                                "/gamer/" + user +
+                                "/matches/" + mode +
+                                "/start/" + start +
+                                "/end/" + end +
+                                "/details/";
+
+            return await AuthenticatedRequest(endpoint);
+        }
+        public async Task<Object> AuthenticatedRequest(string endpoint)
+        {
+            var client = new RestClient("https://my.callofduty.com/api/papi-client/" + endpoint);
+            client.Timeout = -1;
+
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("Cookie", "ACT_SSO_COOKIE=" + auth.sso + "; ACT_SSO_COOKIE_EXPIRY=1591153892430; atkn=" + auth.atkn + ";");
+
+            var queryResult = await client.ExecuteAsync<Object>(request);
+            return queryResult.Data;
+        }
+    }
+}
---
+++ Authenticate.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Net;
+using RestSharp;
+
+namespace CodApi
+{
+    public class Authenticate
+    {
+        public string atkn { get; set; }
+        public string sso { get; set; }
+        public string xsrf { get; set; }
+
+        public Authenticate()
+        {
+        }
+
+        public void Login(string email, string password)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string htmlCode = webClient.DownloadString("https://profile.callofduty.com/cod/login");
+                const string _csrf = "_csrf\" value=\"";
+
+                var start = htmlCode.LastIndexOf(_csrf) + _csrf.Length;
+                var end = start;
+
+                while (htmlCode[end] != '"') end++;
+
+                xsrf = htmlCode.Substring(start, end - start);
+
+                var client = new RestClient("https://s.activision.com/do_login?new_SiteId=activision");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+
+                request.AddHeader("Cookie", "XSRF-TOKEN=" + xsrf + "; new_SiteId=activision;");
+                request.AddParameter("username", email);
+                request.AddParameter("password", password);
+                request.AddParameter("remember_me", "true");
+                request.AddParameter("_csrf", xsrf);
+                IRestResponse response = client.Execute(request);
+
+                string header = (string)response.Headers
+                    .Where(x => x.Name == "Set-Cookie")
+                    .Select(x => x.Value)
+                    .FirstOrDefault();
+
+                string[] words = header.Split(';');
+
+                foreach (var word in words)
+                {
+                    if (word.Contains("atkn="))
+                    {
+                        start = word.LastIndexOf("atkn=") + 5;
+                        end = word.Length;
+                        atkn = word.Substring(start, end - start);
+                    }
+                    else if (word.Contains("ACT_SSO_COOKIE="))
+                    {
+                        start = word.LastIndexOf("ACT_SSO_COOKIE=") + 15;
+                        end = word.Length;
+                        sso = word.Substring(start, end - start);
+                    }
+                }
+            }
+        }
+    }
+}
